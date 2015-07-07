d7a57eae2ee303a4f60969343828061d8d9072be
mosfet <giovanni.panice@gmail.com>
2015-07-08 00:27:25 +0200

    add nodepack

--- include/applayer.h
+++ include/applayer.h
@@ -6,9 +6,10 @@ typedef uint8_t OpCode;
 #define DATA 			((uint8_t)0x02)			//dato di misurazione							###	Nodo Sensore -> Centro Controllo
 /*#####################*/
 
+
 typedef struct payload{
-	uint8_t id; 		//id nodo
-	int32_t val;		//valore
+	uint8_t id; 		//id sensore
+	int16_t val;		//valore
 	uint8_t alarm;		//allarme
 	int32_t ht;			//high th.shold
 	int32_t lt;			//low  th.shold
@@ -16,10 +17,24 @@ typedef struct payload{
 	int8_t  priority;	//priorità
 }Payload;
 
+//128 bit application package
+
 typedef struct aPPpackage{
 
 	OpCode 	code;
 	Payload	payload;
 } APPpackage;
 
+//Pacchetto Centro controllo <-> Nodo Centrale
+typedef struct Nodepack{
+	uint64_t id0;//è necessario?
+	uint32_t id1;
+	uint16_t address;
+	uint64_t secret0;
+	uint64_t secret1;
+	APPpackage pack;
+};
 
+
+//id a 12 byte
+//indirizzi a 16 bit
