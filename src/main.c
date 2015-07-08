e5560d3ced4da8ff0efc42faee7085cb2ec22a10
mosfet <giovanni.panice@gmail.com>
2015-07-08 03:07:28 +0200

    add recive logic from CC - not compil

--- src/main.c
+++ src/main.c
@@ -17,14 +17,17 @@
 #include "task.h"
 #include "timers.h"
 #include "queue.h"
-/* functions */
+/* tasks */
 static xQueueHandle xSwitchQueue;
 static void vRxCenterTxNetTask( void *pvParameters);
 static void vRxNetTxCenterTask( void *pvParameters);
+#else
+/* functions */
+static void reciveFromCenter();
+static void reciveFromNet();
 #endif
 
-
-
+//usb handler
 USBD_HandleTypeDef USBD_Device;
 
 #pragma GCC diagnostic push
@@ -37,7 +40,7 @@ void setupUSB(void);
 void setupBSD(void);
 
 //var
-uint32_t lunghezza;
+uint8_t opcode;
 
 int
 main(int argc, char* argv[])
@@ -62,6 +65,8 @@ main(int argc, char* argv[])
    	vTaskStartScheduler();
    	for( ;; );
 #else
+
+//
 
 #endif
 
@@ -108,16 +113,6 @@ static void vRxNetTxCenterTask( void *pvParameters){
 
 void vApplicationMallocFailedHook( void )
 {
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.
-	pvPortMalloc() is called internally by the kernel whenever a task, queue,
-	timer or semaphore is created.  It is also called by various parts of the
-	demo application.  If heap_1.c or heap_2.c are used, then the size of the
-	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-	to query the size of free heap space that remains (although it does not
-	provide information on how the remaining heap might be fragmented). */
 	taskDISABLE_INTERRUPTS();
 	for( ;; );
 }
@@ -125,15 +120,7 @@ void vApplicationMallocFailedHook( void )
 
 void vApplicationIdleHook( void )
 {
-	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-	task.  It is essential that code added to this hook function never attempts
-	to block in any way (for example, call xQueueReceive() with a block time
-	specified, or call vTaskDelay()).  If the application makes use of the
-	vTaskDelete() API function (as this demo application does) then it is also
-	important that vApplicationIdleHook() is permitted to return to its calling
-	function, because it is the responsibility of the idle task to clean up
-	memory allocated by the kernel to any task that has since been deleted. */
+
 }
 /*-----------------------------------------------------------*/
 
@@ -142,11 +129,40 @@ void vApplicationStackOverflowHook( xTaskHandle pxTask, char *pcTaskName )
 	( void ) pcTaskName;
 	( void ) pxTask;
 
-	/* Run time stack overflow checking is performed if
-	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-	function is called if a stack overflow is detected. */
 	taskDISABLE_INTERRUPTS();
 	for( ;; );
+}
+#else
+
+
+
+static void reciveFromCenter(){
+	//struct NodeMessage message* = malloc(sizeof(struct NodeMessage));
+
+	struct NodeMessage *message;
+	message = (NodeMessage *)malloc(sizeof(NodeMessage));
+
+	if(VCP_read(&message, DIMPACK)!=0){
+		switch(message->code){
+		case READDATA:
+			//invia richiesta di lettura
+			break;
+		case CONFIGSENSOR:
+			//invia configurazione al sensore
+			break;
+		case REPLYJOIN:
+			//risposta alla join
+
+			break;
+		};
+
+	}
+
+}
+
+//callback di livello rete
+static void reciveFromNet(){
+
 }
 #endif
 
